b1db94300e013839fc019ec3bc176e95590f676f
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-08-07 21:18:06 +0530

    Form Validation Revision via JS Project

---
+++ JS Practice/Form Validation/script.js
@@ -0,0 +1,34 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let flag = 1;
+
+function validateForm(){
+    if(username.value === ""){
+        document.getElementById("userError").innerHTML = "User Name is empty";
+        flag = 0;
+    } else if(username.value.length < 3) {
+        document.getElementById("userError").innerHTML = "User Name require min 3 char";
+        flag = 0;
+    } else {
+        document.getElementById("userError").innerHTML = "";
+        flag = 1;
+    }
+
+    if(password.value === ""){
+        document.getElementById("passError").innerHTML = "Password is empty";
+        flag = 0;
+    } else if(password.value.length < 3 & password.value.length > 1){
+        document.getElementById("passError").innerHTML = "Password must be of length 3";
+        flag = 0;
+    } else {
+        document.getElementById("passError").innerHTML = "";
+        flag = 1;
+    }
+
+    if(flag){
+        return true;
+    } else {
+        return false;
+    }
+
+}
